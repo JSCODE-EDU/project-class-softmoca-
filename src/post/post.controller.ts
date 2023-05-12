016513f5abf74589eda2bb303ccdd9bd1b927770
softmoca <acyc0909@gmail.com>
2023-05-12 11:45:53 +0900

    'add_update_API'

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,24 +1,30 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('posts')
 export class PostController {
   constructor(private postservice: PostService) {}
 
-  @Get('/find')
+  @Get()
   getAllPost() {
     return this.postservice.getAllPost();
   }
 
-  @Get('/find/:id')
+  @Get(':id')
   getOnePost(@Param('id') id: number) {
     return this.postservice.getOnePost(id);
   }
 
-  @Post('/create')
+  @Post()
   create(@Body() createPostDto: CreatePostDto) {
     const { title, content } = createPostDto;
     return this.postservice.createPost(title, content);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: number, @Body() updataPostDto: UpdatePostDto) {
+    return await this.postservice.update(id, updataPostDto);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Post } from 'mymodel/entities/Post';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
@@ -22,5 +23,15 @@ export class PostService {
 
   async getOnePost(id: number) {
     return await this.postRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updataPostDto: UpdatePostDto) {
+    const post = await this.getOnePost(id);
+    const { title, content } = updataPostDto;
+
+    post.title = title;
+    post.content = content;
+
+    return await this.postRepository.save(post);
   }
 }
