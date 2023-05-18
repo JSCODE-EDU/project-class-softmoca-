240832fd853c0ed750aaea478b367f616ebe17cc
softmoca <acyc0909@gmail.com>
2023-05-18 09:26:26 +0900

    'feat_cors'

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document); // 이러면  http://localhost/api  로 가면 swagger 볼 수 있다.
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: true, // 개발시에는 treu 그러면 어떤 프론트도 접근가능, 배포시 특정 url
+    credentials: true, // 백과 프론트 모두에서 true로 해줘야한다.
+  });
   await app.listen(3000);
 }
 bootstrap();
